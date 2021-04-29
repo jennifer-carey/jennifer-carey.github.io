bbfa7d0d89e02d131191efa4c971be1599566413
Jennifer <jennifacarey@gmail.com>
2021-04-29 12:32:50 +0100

    burger menu

---
+++ app.js
@@ -0,0 +1,10 @@
+const navSlide = () => {
+    const burger = document.querySelector(".menu-icon");
+    const nav = document.querySelector(".nav-list");
+
+    burger.addEventListener("click", ()=> {
+        nav.classList.toggle("nav-active");
+    });
+}
+
+navSlide();
