d59b0e7a729035a756f0629e6eb8b0b98501ca23
Jennifer Carey <jennifacarey@gmail.com>
2021-06-26 13:36:01 +0100

    hide menu on link click

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const hamburger = document.querySelector("#hamburger");
 const navigation = document.querySelector("#nav");
+const navLinks = document.querySelectorAll(".nav-link");
 
-hamburger.addEventListener("click", () => {
+function closeNav() {
+  navigation.classList.remove("show");
+  hamburger.classList.remove("open");
+}
+
+function toggleMenu() {
   navigation.classList.toggle("show");
   hamburger.classList.toggle("open");
-});
+}
+
+hamburger.addEventListener("click", toggleMenu);
+navLinks.forEach(link => link.addEventListener("click", closeNav));
+
+
 
